c964cda6734bce17c07c68078e860a9ff397160c
Josh Nesbitt <josh@stac.works>
2026-02-28 08:35:39 +0000

    Make thoughts list better.

--- src/utils/finders.js
+++ src/utils/finders.js
@@ -1,19 +1,21 @@
 import { getCollection } from 'astro:content';
 
-async function getThoughts() {
+async function getThoughts(limit) {
 	const collection = await getCollection('thought')
 
 	return collection
 		.filter((rec) => rec.data.published)
-		.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate));
+		.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate))
+		.slice(0, limit);
 }
 
-async function getReviews() {
+async function getReviews(limit) {
 	const collection = await getCollection('review')
 
 	return collection
 		.filter((rec) => rec.data.published)
-		.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate));
+		.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate))
+		.slice(0, limit);
 }
 
 export {
